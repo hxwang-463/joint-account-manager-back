b49ddf17e305b15f61bf553970ea7261e44a9e32
Francis W <whx987172@gmail.com>
2025-04-03 12:38:38 -0500

    fix cron jobs; bump to 1.2.0

--- src/main/java/xyz/hxwang/jointaccountmanager/JointAccountManagerApplication.java
+++ src/main/java/xyz/hxwang/jointaccountmanager/JointAccountManagerApplication.java
@@ -2,7 +2,9 @@ package xyz.hxwang.jointaccountmanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
 @SpringBootApplication
 public class JointAccountManagerApplication {
 
